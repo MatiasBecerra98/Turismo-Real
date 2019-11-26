b36ff691ee7d57eadf764b949915ffa355a141d2
Victoriosu <seb.echeverria@alumnos.duoc.cl>
2019-11-25 22:12:06 -0300

    Added qualities to appartment json

--- app/models/appartment.rb
+++ app/models/appartment.rb
@@ -2,4 +2,21 @@
 class Appartment < ApplicationRecord
   has_one :appartment_quality
   has_many :reservations
+
+  def cable
+    self.appartment_quality.try(:cable) || false
+  end
+
+  def conditioned_air
+    self.appartment_quality.try(:conditioned_air) || false
+  end
+
+  def internet
+    self.appartment_quality.try(:internet) || false
+  end
+
+  def calefaction
+    self.appartment_quality.try(:calefaction) || false
+  end
+
 end
--- app/views/appartments/_appartment.json.jbuilder
+++ app/views/appartments/_appartment.json.jbuilder
@@ -1,3 +1,3 @@
 json.extract! appartment, :id, :nightly_fee, :name, :address, :created_at,
-              :updated_at, :appartment_quality, :reservations
+              :updated_at, :reservations, :cable, :conditioned_air, :internet, :calefaction
 json.url appartment_url(appartment, format: :json)
