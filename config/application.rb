93cc367989aece27ab0db90507e446448e0aed58
Matias <ma.becerrah@alumnos.duoc.cl>
2019-11-26 00:41:19 -0300

    precompile

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module A
     config.autoload_paths << Rails.root.join("lib")
     config.eager_load_paths << Rails.root.join("lib")
 
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
