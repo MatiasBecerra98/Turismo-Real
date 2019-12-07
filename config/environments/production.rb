4edab9a1899289c93a16b625bc823e7aa3e5f119
Matias <ma.becerrah@alumnos.duoc.cl>
2019-12-07 01:13:34 -0300

    Add default url option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,7 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
   config.active_storage.service = :amazon
+  Rails.application.routes.default_url_options[:host] = 'turismo-real.herokuapp.com'
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
