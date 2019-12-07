9194c623528aed006281be5ec70799681968d0ef
Matias <ma.becerrah@alumnos.duoc.cl>
2019-12-07 06:48:57 -0300

    add appartment to reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,22 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :appartment
+
+  def appartment
+    appartment = Appartment.find(appartment_id)
+    {
+      id: appartment.id,
+      nightly_fee: appartment.nightly_fee,
+      name: appartment.name,
+      description: appartment.description,
+      addres: appartment.address,
+      created_at: appartment.created_at,
+      updated_at: appartment.updated_at,
+      cabl: appartment.cable,
+      conditioned_air: appartment.conditioned_air,
+      internet: appartment.internet,
+      calefaction: appartment.calefaction,
+      image_url: appartment.image_url
+    }
+  end
 end
